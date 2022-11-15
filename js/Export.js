aaf66997a23bf6e5a1efba5b971f6f6750fb3343
chanyoungs <chanyoungs@gmail.com>
2022-11-15 22:28:58 +0000

    Import script from download folder

--- js/Export.js
+++ js/Export.js
@@ -1,37 +1,8 @@
-//@include "_utils.js"
+var doc = app.activeDocument;
 if (doc.saved) {
-    progress(bulletins.length * (1 + 3));
-    progress.message("Initialising...");
-    var getNameWithoutExtension = function (doc) {
-        var fullName = doc.name;
-        var finalDotPosition = fullName.lastIndexOf(".");
-        return finalDotPosition > -1
-            ? fullName.substr(0, finalDotPosition)
-            : fullName;
-    };
-    var pdfExfortPreset = app.pdfExportPresets.add({});
-    // Export Web PDFs
-    pdfExfortPreset.exportReaderSpreads = false;
-    for (var b = 0; b < bulletins.length; b++) {
-        progress.message("Exporting Web PDFs: (".concat(b + 1, "/").concat(bulletins.length, ")"));
-        app.pdfExportPreferences.pageRange = "".concat(b * 4 + 1, "-").concat(b * 4 + 4);
-        doc.exportFile(ExportFormat.PDF_TYPE, File("".concat(myPath, "/").concat(getNameWithoutExtension(doc), "-Web-").concat(bulletins[b], ".pdf")), false, pdfExfortPreset);
-        progress.increment();
-    }
-    // // Export Print view
-    pdfExfortPreset.exportReaderSpreads = true;
-    var flipSpread = function (leftPageIndex) {
-        doc.pages[leftPageIndex].move(LocationOptions.AFTER, doc.pages[leftPageIndex + 1], BindingOptions.RIGHT_ALIGN);
-        doc.pages[leftPageIndex].move(LocationOptions.BEFORE, doc.pages[leftPageIndex], BindingOptions.LEFT_ALIGN);
-    };
-    for (var b = 0; b < bulletins.length; b++) {
-        progress.message("Exporting Print PDFs: (".concat(b + 1, "/").concat(bulletins.length, ")"));
-        flipSpread(b * 4);
-        progress.increment();
-        app.pdfExportPreferences.pageRange = "".concat(b * 4 + 1, "-").concat(b * 4 + 4);
-        doc.exportFile(ExportFormat.PDF_TYPE, File("".concat(myPath, "/").concat(getNameWithoutExtension(doc), "-Print-").concat(bulletins[b], ".pdf")), false, pdfExfortPreset);
-        progress.increment();
-        flipSpread(b * 4);
-        progress.increment();
-    }
+    var myPath = doc.fullName.parent.fsName.toString().replace(/\\/g, "/");
+    eval("#include '".concat(myPath, "/_scripts/Merge.js'"));
+}
+else {
+    alert("Please save document first");
 }
---
+++ ts/_export.ts
@@ -0,0 +1,72 @@
+//@include "_utils.js"
+
+if (doc.saved) {
+    progress(bulletins.length * (1 + 3))
+    progress.message("Initialising...")
+
+    const getNameWithoutExtension = (doc: Document) => {
+        const fullName = doc.name
+        const finalDotPosition = fullName.lastIndexOf(".")
+        return finalDotPosition > -1
+            ? fullName.substr(0, finalDotPosition)
+            : fullName
+    }
+
+    const pdfExfortPreset = app.pdfExportPresets.add({})
+
+    // Export Web PDFs
+    pdfExfortPreset.exportReaderSpreads = false
+    for (let b = 0; b < bulletins.length; b++) {
+        progress.message(`Exporting Web PDFs: (${b + 1}/${bulletins.length})`)
+        app.pdfExportPreferences.pageRange = `${b * 4 + 1}-${b * 4 + 4}`
+        doc.exportFile(
+            ExportFormat.PDF_TYPE,
+            File(
+                `${myPath}/${getNameWithoutExtension(doc)}-Web-${
+                    bulletins[b]
+                }.pdf`
+            ),
+            false,
+            pdfExfortPreset
+        )
+        progress.increment()
+    }
+
+    // // Export Print view
+    pdfExfortPreset.exportReaderSpreads = true
+
+    const flipSpread = (leftPageIndex: number) => {
+        doc.pages[leftPageIndex].move(
+            LocationOptions.AFTER,
+            doc.pages[leftPageIndex + 1],
+            BindingOptions.RIGHT_ALIGN
+        )
+        doc.pages[leftPageIndex].move(
+            LocationOptions.BEFORE,
+            doc.pages[leftPageIndex],
+            BindingOptions.LEFT_ALIGN
+        )
+    }
+
+    for (let b = 0; b < bulletins.length; b++) {
+        progress.message(`Exporting Print PDFs: (${b + 1}/${bulletins.length})`)
+        flipSpread(b * 4)
+        progress.increment()
+
+        app.pdfExportPreferences.pageRange = `${b * 4 + 1}-${b * 4 + 4}`
+        doc.exportFile(
+            ExportFormat.PDF_TYPE,
+            File(
+                `${myPath}/${getNameWithoutExtension(doc)}-Print-${
+                    bulletins[b]
+                }.pdf`
+            ),
+            false,
+            pdfExfortPreset
+        )
+        progress.increment()
+
+        flipSpread(b * 4)
+        progress.increment()
+    }
+}
